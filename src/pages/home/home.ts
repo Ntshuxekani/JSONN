4fa1a9d220b3827febe2a433c9fa9abbca62297e
ntshuxi <ntshuxekanizitha@gmail.com>
2018-08-20 15:25:13 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,5 @@
+import { DetailsPage } from './../details/details';
+import { CourseServiceProvider } from './../../providers/course-service/course-service';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -6,9 +8,16 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+courses:any
+arrCourses=[]
+  constructor(public navCtrl: NavController, private courseProvider:CourseServiceProvider) {
+courseProvider.getCourses().subscribe(results =>{
+  this.courses=results;
+this.arrCourses=this.courses.courses
+console.log("response", results)
+});
   }
-
-}
+  next(course:any){
+    this.navCtrl.push(DetailsPage,{value:course});
+   }
+   }
